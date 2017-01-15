fc7a898b1c361a8928b9ecd766179c051f9ad767
markus-wi <emveyh@googlemail.com>
2017-01-15 20:05:01 +0100

    added first draft implementation of authorization handling

--- src/js/authentication/authentication.js
+++ src/js/authentication/authentication.js
@@ -4,19 +4,19 @@ const Authentication = module.exports = {};
 
 Authentication.passportStrategy = null;
 
-Authentication.createAuthenticationHandler = function createAuthenticationHandler(loginRequired = true) {
+Authentication.createAuthenticationHandler = function createAuthenticationHandler(loginRequired = true, operation, authorizationHandler) {
 	let result = null;
 	if (loginRequired === false || !this.passportStrategy) {
 		result = (req, res, next) => next();
 	} else {
 		result = (req, res, next) => {
-			this.authenticate(req, res, next);
+			this.authenticate(req, res, next, operation, authorizationHandler);
 		};
 	}
 	return result;
 };
 
-Authentication.authenticate = function authenticate(req, res, next) {
+Authentication.authenticate = function authenticate(req, res, next, operation, authorizationHandler) {
 	passport.authenticate(this.passportStrategy, (error, user) => {
 		if (error) {
 			console.log(error);
@@ -25,7 +25,17 @@ Authentication.authenticate = function authenticate(req, res, next) {
 			res.send(401, { error: "Full authentication is required to access this resource" });
 		} else {
 			req.user = user;
-			next();
+			if (authorizationHandler && operation) {
+				authorizationHandler(req, operation, user, (result) => {
+					if (result) {
+						next();
+					} else {
+						res.send(403, { error: "Forbidden" });
+					}
+				});
+			} else {
+				next();
+			}
 		}
 	})(req, res, next);
 };
